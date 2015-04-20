cb1c0d671ef9b541227d618cd44836d4f949dbca
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-20 16:31:00 -0400

    setup routes

--- client/app/zephyr.js
+++ client/app/zephyr.js
@@ -1,6 +1,28 @@
-angular.module('zephyr', ['ngRoute', 'ngCookies', 'ui.bootstrap']);
+angular.module('zephyr', ['ngRoute', 'ngCookies', 'ui.bootstrap', 'ui.router']);
 angular.module('zephyr').config(Configuration);
 
-function Configuration() {
-   
+function Configuration($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "partials/flight_entry.html"
+        })
+        .state('search', {
+            url: "/search",
+            templateUrl: "partials/flight_search.html"
+          
+        })
+        .state('search.list', {
+            url: "/state2",
+            templateUrl: "partials/flight_search.list.html"
+        })
+        .state('airport', {
+            url: "/airport",
+            templateUrl: "partials/airport.html"
+            
+        })
+        .state('time', {
+            url: "/time", 
+            templateUrl: "partials/time.html"
+        });
 }
