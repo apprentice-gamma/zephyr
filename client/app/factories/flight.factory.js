e506ae4ad9d2ccd21ec597bace4da596dcfff09f
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-22 11:13:18 -0400

    parsing the json data

--- client/app/factories/flight.factory.js
+++ client/app/factories/flight.factory.js
@@ -31,25 +31,32 @@
       console.log(url);
       $http.jsonp(url).
         success(function(data, status, headers, config) {
-    			// factory.depAirportLoc.lat = data.departureAirport.latitude;
-    			// factory.depAirportLoc.lon = data.departureAirport.longitude;
-    			// factory.arrAirportLoc.lat = data.arrivalAirport.latitude;
-    			// factory.arrAirportLoc.lon = data.arrivalAirport.longitude;
-    			// factory.schlGateDep = data.operationalTimes.scheduledGateDeparture.dateUtc;
-    			// factory.schlGateArr = data.operationalTimes.estimatedGateArrival.dateUtc;
-
+        	findAirports(data.appendix.airports);
+        	factory.flightStatus = data.flightStatuses[0];
+        	factory.flightTimes = data.flightStatuses[0].operationalTimes;
+        	
+			// factory.depAirportLoc.lat = data.departureAirport.latitude;
+			// factory.depAirportLoc.lon = data.departureAirport.longitude;
+			// factory.arrAirportLoc.lat = data.arrivalAirport.latitude;
+			// factory.arrAirportLoc.lon = data.arrivalAirport.longitude;
+			// factory.schlGateDep = data.operationalTimes.scheduledGateDeparture.dateUtc;
+			// factory.schlGateArr = data.operationalTimes.estimatedGateArrival.dateUtc;
 		      console.log(data);
-
-          // if(data.owned_households !== undefined)                
-          //         factory.flight = data.owned_households;
-          // else
-          //         factory.flight = data[0].owned_households;
-          // 
-          // error(function(data, status, headers, config) {
-          //     console.log("There was an error retrieving JSON data");// log error
-          // });
     		});
     } 
+
+    function findAirports(airports) {
+    	var fsDep = factory.flightStatuses.departureAirportFsCode;
+    	var fsArr = factory.flightStatuses.arrivalAirportFsCode;
+
+    	for (var i = 0; i < airports.length; i++) {
+    		if(airports[i].fs === fsDep) {
+    			factory.departureAirport = airports[i]
+    		} else if(airports[i].fs === fsArr) {
+    			factory.arrivalAirport = airports[i]
+    		}
+    	}
+    }
 
     function parseFlightNumber(flightNumber) {
       var match = /\d/.exec(flightNumber);
