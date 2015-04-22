f8b1b533f03f669c1bc81b8f93357b16351202de
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-22 13:53:16 -0400

    countdown timer working with actual current time

--- client/app/controllers/flightTracker.controller.js
+++ client/app/controllers/flightTracker.controller.js
@@ -7,13 +7,13 @@
 		var vm = this;
 		vm.FlightFactory = FlightFactory;
 		vm.ActivityFactory = ActivityFactory;
-		vm.countdown = 0;
 		vm.today = new Date();
-		vm.trackUTC = '2015-04-22T23:00:00.000Z';
-		$scope.$broadcast('timer-start');
-		var banana = calculateCountdown(vm.trackUTC);
+		vm.trackUTC = '2015-04-22T23:00:00.000Z';		
+		vm.countdown = calculateCountdown(vm.trackUTC);
+		console.log("it's the final countdown",vm.countdown)
 
 		function calculateCountdown(input) {
+			console.log("Calculation made")
 			var d = new Date(input);
 			var now = new Date();
 			return (d.getTime() - now.getTime()) / 60000;
