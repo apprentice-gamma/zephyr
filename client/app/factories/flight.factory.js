81916222f0b2fd9391315a2cf260542d26f87366
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-22 10:22:51 -0400

    JSON to JSONP request

--- client/app/factories/flight.factory.js
+++ client/app/factories/flight.factory.js
@@ -13,8 +13,8 @@
 		factory.flightComponents = {};
 		factory.today = "2015/4/21";
 		// factory.apiurl = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/DAL/2128/DEP/2015/4/21?appId=588e049b&appKey=f9e4c706444bfc87888b78ddb64f00c8&utc=false";
-		factory.apibase = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/";
-		factory.suffix = "?appId=588e049b&appKey=f9e4c706444bfc87888b78ddb64f00c8&utc=false";
+		factory.apibase = "https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/status/";
+		factory.suffix = "?callback=JSON_CALLBACK&appId=588e049b&appKey=f9e4c706444bfc87888b78ddb64f00c8&utc=false";
 
 		//methods
     factory.getFlightData = getFlightData;
@@ -29,15 +29,14 @@
     	url += factory.today;
     	url += factory.suffix;
       console.log(url);
-
-      $http.get(url).
+      $http.jsonp(url).
         success(function(data, status, headers, config) {
-    			factory.depAirportLoc.lat = data.departureAirport.latitude;
-    			factory.depAirportLoc.lon = data.departureAirport.longitude;
-    			factory.arrAirportLoc.lat = data.arrivalAirport.latitude;
-    			factory.arrAirportLoc.lon = data.arrivalAirport.longitude;
-    			factory.schlGateDep = data.operationalTimes.scheduledGateDeparture.dateUtc;
-    			factory.schlGateArr = data.operationalTimes.estimatedGateArrival.dateUtc;
+    			// factory.depAirportLoc.lat = data.departureAirport.latitude;
+    			// factory.depAirportLoc.lon = data.departureAirport.longitude;
+    			// factory.arrAirportLoc.lat = data.arrivalAirport.latitude;
+    			// factory.arrAirportLoc.lon = data.arrivalAirport.longitude;
+    			// factory.schlGateDep = data.operationalTimes.scheduledGateDeparture.dateUtc;
+    			// factory.schlGateArr = data.operationalTimes.estimatedGateArrival.dateUtc;
 
 		      console.log(data);
 
