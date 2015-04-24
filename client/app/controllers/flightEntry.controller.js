9c307e489ab10bab3a074963e31c4dd8ebccbf59
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-24 13:47:19 -0400

    typeahead

--- client/app/controllers/flightEntry.controller.js
+++ client/app/controllers/flightEntry.controller.js
@@ -2,11 +2,13 @@
     angular
         .module('zephyr')
         .controller('flightEntry', flightEntry);
-    function flightEntry(FlightFactory, SpeechService, DirectionFactory, $state, $geolocation, $modalStack, $http) {
+    function flightEntry(FlightFactory, SpeechService, DirectionFactory, AirportFactory, $state, $geolocation, $modalStack, $http) {
         var vm = this;
         vm.FlightFactory = FlightFactory;
         vm.trackFlight = trackFlight;
         vm.SpeechService = SpeechService;
+        vm.AirportFactory = AirportFactory;
+        vm.airportCodes = Object.keys(AirportFactory.airports);
         vm.listenCommand = listenCommand;
         vm.airport = "";  
 
