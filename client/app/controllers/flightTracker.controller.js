48517521431efda3de0be9d5361f5e3fcee48dd1
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-24 13:00:05 -0400

    ng-class

--- client/app/controllers/flightTracker.controller.js
+++ client/app/controllers/flightTracker.controller.js
@@ -8,16 +8,28 @@
 		var second = 1000;
 		var minute = second * 60;
 		var showWait = false;
+		var flightSoon = false;
 
 		vm.FlightFactory = FlightFactory;
 		vm.ActivityFactory = ActivityFactory;
 		vm.carCountdown = FlightFactory.calculateCountdown(DirectionFactory.drivingETA);
-		vm.DirectionFactory = DirectionFactory;
-		vm.flightCountdown = FlightFactory.calculateCountdown(FlightFactory.connectionTime);		
+		vm.flightCountdown = FlightFactory.calculateCountdown(FlightFactory.connectionTime);
+		vm.changeTimerClass = changeTimerClass;	
 
 		if ($state.is('track')) {
 			setInterval(intervalFunction, 5 * minute);
 			showWait = FlightFactory.showWait;
+
+			if (vm.flightCountdown <= 30) {
+				flightSoon = true;
+			}
+		}
+
+		function changeTimerClass(){
+			if(flightSoon)
+				return "timer-warning";
+			else
+				return "timer";
 		}
 
 		function intervalFunction(){
